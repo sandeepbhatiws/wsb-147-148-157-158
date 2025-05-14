1d647e6e92c1144adf7614bcc64053dcd07508d5
pradeeprai1990 <pradeep.9997@gmail.com>
2025-05-14 18:27:09 +0530

    Chanegs

--- React Js/accordian/src/Accordian.jsx
+++ React Js/accordian/src/Accordian.jsx
@@ -1,38 +1,39 @@
 import React, { useState } from 'react'
 import data from './faqdata.js';
+import FaqHeading from './FaqHeading.jsx';
+import FaqSection from './FaqSection.jsx';
 
 export default function Accordian() {
 
     const [faqs, setFaqs] = useState(data);
     const [currentIndex, setCurrentIndex] = useState(0);
 
-    const showFaq = (i) => {
-        if(currentIndex == i){
-            setCurrentIndex();
-        } else  {
-            setCurrentIndex(i);
-        }
-    }
+    
 
     return (
         <>
             <div class="outer" id="outer">
-                <div class="heading">
-                    <h1>Frequently Asked Questions</h1>
-                </div>
+                {/* <FaqHeading heading="Frequently Asked Questions" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut cum quae qui harum veritatis amet, placeat excepturi ducimus reprehenderit vero. Ut vitae cupiditate, repellendus odio minima laboriosam dolorem error? Sunt." /> */}
+
+                <FaqHeading heading="Frequently Asked Questions" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut cum quae qui harum veritatis amet, placeat excepturi ducimus reprehenderit vero. Ut vitae cupiditate, repellendus odio minima laboriosam dolorem error? Sunt.">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut cum quae qui harum veritatis amet, placeat excepturi ducimus reprehenderit vero. Ut vitae cupiditate, repellendus odio minima laboriosam dolorem error? Sunt.
+                </FaqHeading>
 
                 {
                     faqs.map((value, index) => {
                         return(
-                            <div class="outer_faq" key={index}>
-                                <div class="question" onClick={ () => showFaq(index) }>
-                                    { value.question }
-                                    <span>{ (currentIndex == index) ? '-' : '+'  }</span>
-                                </div>
-                                <div class={ (currentIndex == index) ? 'answer' : 'answer hidden'  }>
-                                    { value.answer }
-                                </div>
-                            </div>
+                            <FaqSection faqData={value} key={index} currentIndex={currentIndex} index={index} setCurrentIndex={setCurrentIndex} />
+
+
+                            // <div class="outer_faq" key={index}>
+                            //     <div class="question" onClick={ () => showFaq(index) }>
+                            //         { value.question }
+                            //         <span>{ (currentIndex == index) ? '-' : '+'  }</span>
+                            //     </div>
+                            //     <div class={ (currentIndex == index) ? 'answer' : 'answer hidden'  }>
+                            //         { value.answer }
+                            //     </div>
+                            // </div>
                         )
                     })
                 }
