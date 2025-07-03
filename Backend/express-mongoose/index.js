c13dcc8a3f2b737297382f2e74a8c61d868ce827
pradeeprai1990 <pradeep.9997@gmail.com>
2025-07-03 18:10:42 +0530

    Changes

---
+++ Backend/express-mongoose/index.js
@@ -0,0 +1,29 @@
+const express =  require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// Executable Function
+const server = express();
+
+// parse requests of content-type - application/json
+server.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+server.use(express.urlencoded({ extended: true }));
+
+server.use(bodyParser.json());
+server.use(cors());
+
+server.get('/', (request, response) => {
+    response.send('Server is working fine !');
+});
+
+
+server.listen(8001, () => {
+    mongoose.connect('mongodb://127.0.0.1:27017/wsb_offilne')
+    .then(() => console.log('Connected!'))
+    .catch((error) => {
+        console.log(error);
+    });
+});
